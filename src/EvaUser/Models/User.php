fff9e3e6c64b1d1b32899b756b96791a39251b35
mr5 <u@mr-5.cn>
2014-10-16 17:11:59 +0800

    api 登录返回 screenName 字段

--- src/EvaUser/Models/User.php
+++ src/EvaUser/Models/User.php
@@ -14,6 +14,7 @@ class User extends Entities\Users
         'email',
         'status',
         'emailStatus',
+        'screenName',
         'avatar' => 'getAvatar',
         'roles' => 'getRoles',
     );
