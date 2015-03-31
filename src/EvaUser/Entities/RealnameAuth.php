4b4d6e982aa4a48c19d5d687b804995dbaa5f901
HaoHong <haohong725@qq.com>
2015-03-31 17:15:10 +0800

    实名认证，前台

--- src/EvaUser/Entities/RealnameAuth.php
+++ src/EvaUser/Entities/RealnameAuth.php
@@ -8,6 +8,18 @@ use Eva\EvaEngine\Exception;
  */
 class RealnameAuth extends Model
 {
+    //身份证号码不存在
+    const STATUS_UNDEFINED = 1;
+
+    //身份证号号码与姓名不一致
+    const STATUS_UNMATCH = 2;
+
+    //验证成功
+    const STATUS_SUCCESS = 3;
+
+    //已提交，验证中
+    const STATUS_VERIFYING = 4;
+
     public $userId;
     /**
      * 姓名
@@ -21,7 +33,7 @@ class RealnameAuth extends Model
      * @SWG\Property
      */
     public $cardNum;
-    public $status = 0;
+    public $status = 4;
     public $createTime;
     protected $tableName = 'user_realname_auth';
 
