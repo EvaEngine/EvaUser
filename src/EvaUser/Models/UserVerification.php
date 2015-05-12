9bad51fa7acf96a46b5fe0d1000a097de9b73c6b
categorical <dawei.chen@hotmail.co.uk>
2015-05-12 17:33:58 +0800

    combine verfication apis

---
+++ src/EvaUser/Models/UserVerification.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Eva\EvaUser\Entities;
+
+use \Eva\EvaEngine\Mvc\Model;
+use Eva\EvaEngine\Exception;
+
+/**
+ * @SWG\Model(id="UserVerification")
+ */
+class UserVerification extends Model
+{
+    /**
+     * @SWG\Property(
+     * description="a mobile number or an email address"),
+     * required=true
+     * @var
+     */
+    public $identifier;
+    /**
+     * @SWG\Property(
+     * enum="['EMAIL', 'MOBILE']"),
+     * required=true
+     * @var
+     */
+    public $identifierType;
+
+    /**
+     * @SWG\Property(
+     * name="role",type="string",
+     * enum="['USR_REGISTER', 'USR_EDIT']",
+     * description="The purpose of this verification. Useful when customising verification templates at different stages."),
+     * required=true
+     * @var
+     */
+    public $role;
+
+
+}
