e0e9348dc1aabaa02354aa10c2b788d31231e1fa
FrankTung <franktung7@gmail.com>
2015-08-11 10:52:39 +0800

    login records

---
+++ src/EvaUser/Models/LoginRecord.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Wscn
+ *
+ * @link: https://github.com/wallstreetcn/wallstreetcn
+ * @author: franktung<franktung@gmail.com>
+ * @Date: 15/8/11
+ * @Time: 上午10:32
+ *
+ */
+
+namespace Eva\EvaUser\Models;
+
+
+use Eva\EvaUser\Entities\LoginRecords;
+
+class LoginRecord extends LoginRecords
+{
+
+} 
