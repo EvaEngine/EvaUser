a226b485a23ec4fb777847327f9d6d8937c77584
Decent Woo <u@mr-5.cn>
2015-08-24 16:40:25 +0800

    add clean cookies action

--- src/EvaUser/Controllers/LogoutController.php
+++ src/EvaUser/Controllers/LogoutController.php
@@ -15,4 +15,14 @@ class LogoutController extends ControllerBase
 
         return $this->response->redirect('/admin');
     }
+    public function cleancookiesAction()
+    {
+        if (!empty($_COOKIE)) {
+            $sessionDomain = $this->getDI()->getConfig()->session->cookie_params->domain;
+            foreach ($_COOKIE as $key => $cookie) {
+                $this->cookies->get($key)->setDomain($sessionDomain)->delete();
+            }
+        }
+        return $this->response->redirect(eva_url('passport', '/login'));
+    }
 }
