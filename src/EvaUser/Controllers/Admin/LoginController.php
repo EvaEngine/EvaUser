90d0a3b2d7a4543255f2572e70b9ec0462064b63
Decent Woo <u@mr-5.cn>
2015-08-24 13:36:41 +0800

    redirect /admin/login to passport

--- src/EvaUser/Controllers/Admin/LoginController.php
+++ src/EvaUser/Controllers/Admin/LoginController.php
@@ -10,6 +10,14 @@ class LoginController extends ControllerBase
 {
     public function indexAction()
     {
+        $next = eva_url('admin', '/admin/dashboard');
+        if ($this->request->getHTTPReferer()) {
+            $next = $this->request->getHTTPReferer();
+        }
+
+        return $this->response->redirect(eva_url('passport', '/login', [
+            'next' => $next
+        ]));
         $this->view->changeRender('admin/login/index');
         $this->dispatcher->setParam('loginSuccessRedirectUri', '/admin/dashboard');
 
